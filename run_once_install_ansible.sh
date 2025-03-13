31feb0831ac2ea02e24e3e3ea3997d543ec50375
Stefan <master2005pop@gmail.com>
2025-03-13 14:03:46 +0100

    Initial commit

---
+++ run_once_install_ansible.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+
+install_on_ubuntu() {
+	sudo apt-get update
+	sudo apt-get install -y ansible
+}
+
+OS="$(uname -s)"
+case "${OS}" in
+	Linux*)
+		if [ -f /etc/lsb-release ]; then
+			install_on_ubuntu
+		else
+			echo "Unsupported Linux distribution."
+			exit 1
+		fi
+		;;
+	*)
+		echo "Unsupported operating system: ${OS}"
+		exit 1
+		;;
+esac
+
+ansible-playbook ~/.bootstrap/setup.yml --ask-become-pass
+ansible-playbook ~/.bootstarp/setup_flatpak.yml
+
+echo "Ansible installation complete."
---
+++ run_once_install_ghostty.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Getting Ghostty"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/mkasberg/ghostty-ubuntu/HEAD/install.sh)" 
