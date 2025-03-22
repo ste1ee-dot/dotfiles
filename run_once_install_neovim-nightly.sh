7d70abb59c2777fe287c32d032f17dc5d50e1615
Stefan <master2005pop@gmail.com>
2025-03-22 20:50:45 +0100

    Fixed neovim-nightly install

--- run_once_install_neovim-nightly.sh
+++ run_once_install_neovim-nightly.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-/bin/bash -c "$(curl -fsSL https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.appimage)"
+# Download the Neovim Nightly AppImage
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-linux-x86_64.appimage
+
+# Make the AppImage executable
+chmod u+x nvim-linux-x86_64.appimage
+
+sudo mv nvim-linux-x86_64.appimage /usr/local/bin/nvim
