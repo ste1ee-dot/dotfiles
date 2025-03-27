c93348fa56bd21240f31e71261d522835ff071c6
Stefan <master2005pop@gmail.com>
2025-03-27 21:37:21 +0100

    Added html & cssls lsps

--- dot_config/nvim/after/plugin/mason.lua
+++ dot_config/nvim/after/plugin/mason.lua
@@ -3,31 +3,36 @@ require('mason').setup()
 
 -- Initialize mason-lspconfig
 require('mason-lspconfig').setup({
-  -- List the language servers you want to install
-  ensure_installed = { 'gopls', 'lua_ls' },
-  handlers = {
-    -- Default handler for all language servers
-    function(server_name)
-      require('lspconfig')[server_name].setup({})
-    end,
-    -- Custom handler for lua_ls
-    lua_ls = function()
-      local lua_opts = {
-        settings = {
-          Lua = {
-            runtime = {
-              version = 'LuaJIT',
-            },
-            diagnostics = {
-              globals = { 'vim' },
-            },
-            workspace = {
-              library = vim.api.nvim_get_runtime_file("", true),
-            },
-          },
-        },
-      }
-      require('lspconfig').lua_ls.setup(lua_opts)
-    end,
-  },
+    -- List the language servers you want to install
+    ensure_installed = {
+        'gopls',
+        'lua_ls',
+        'html',
+        'cssls',
+    },
+    handlers = {
+        -- Default handler for all language servers
+        function(server_name)
+            require('lspconfig')[server_name].setup({})
+        end,
+        -- Custom handler for lua_ls
+        lua_ls = function()
+            local lua_opts = {
+                settings = {
+                    Lua = {
+                        runtime = {
+                            version = 'LuaJIT',
+                        },
+                        diagnostics = {
+                            globals = { 'vim' },
+                        },
+                        workspace = {
+                            library = vim.api.nvim_get_runtime_file("", true),
+                        },
+                    },
+                },
+            }
+            require('lspconfig').lua_ls.setup(lua_opts)
+        end,
+    },
 })
